8dcd96af99199d960541b63f3c2253a4e765bad3
Kaushal Kishore <kaushalk@Divyas-MBP.fios-router.home>
2018-11-18 23:48:52 -0500

    change 4

--- scripts/runServers.sh
+++ scripts/runServers.sh
@@ -1,6 +1,11 @@
 #! /bin/bash
 echo "Run Spring Boot Jars...."
-nohup java -Dserver.port=8761 -jar spring-boot-microservice-eureka-naming-server-0.0.1-SNAPSHOT.jar --spring-config.location=./Eureka/application.properties &
-nohup java -Dserver.port=8080 -jar microsevice-connected-0.0.1-SNAPSHOT.jar --spring-config.location=./connected/application.properties &
-nohup java -Dserver.port=8082 -jar microservice-connected-cities-0.0.1-SNAPSHOT.jar --spring-config.location=./connectedCities1/application.properties &
-nohup java -Dserver.port=8083 -jar microservice-connected-cities-0.0.1-SNAPSHOT.jar --spring-config.location=./connectedCities2/application.properties &
+rm pid.txt
+nohup java -Dserver.port=8761 -jar spring-boot-microservice-eureka-naming-server-0.0.1-SNAPSHOT.jar -Dspring-config.location=./Eureka/application.properties &
+echo $! >> pid.txt
+nohup java -Dserver.port=8080 -jar microsevice-connected-0.0.1-SNAPSHOT.jar -Dspring-config.location=./connected/application.properties &
+echo $! >> pid.txt
+nohup java -Dserver.port=8082 -jar microservice-connected-cities-0.0.1-SNAPSHOT.jar -Dspring-config.location=./connectedCities1/application.properties &
+echo $! >> pid.txt
+nohup java -Dserver.port=8083 -jar microservice-connected-cities-0.0.1-SNAPSHOT.jar -Dspring-config.location=./connectedCities2/application.properties &
+echo $! >> pid.txt
--- scripts/stopServers.sh
+++ scripts/stopServers.sh
@@ -1,7 +1,4 @@
 #! /bin/bash
-pid1=`ps aux | grep microservice-connected-cities-0.0.1-SNAPSHOT.jar | awk '{print $2}'`
-kill -9 $pid1
-pid2=`ps aux | grep "spring-boot-microservice-eureka-naming-server-0.0.1-SNAPSHOT.jar" | awk '{print $2}'`
-kill -9 $pid2
-pid3=`ps aux | grep "microsevice-connected-0.0.1-SNAPSHOT.jar" | awk '{print $2}'`
-kill -9 $pid3
+while read pid; do
+    kill -9 $pid
+done < pid.txt
